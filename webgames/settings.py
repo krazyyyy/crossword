edd8d6d71bfbfafd6cc0d58f14e44de6716fa635
Krazy0 <57904740+krazyyyy@users.noreply.github.com>
2021-04-20 05:46:48 +0500

    Update settings.py

--- webgames/settings.py
+++ webgames/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'p+^(b@c&8y-mrzi4z_9@&1_lc8!h^7*ef@0d!y6b*nz#zcp54c'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['krazynetwork.herokuapp.com']
+ALLOWED_HOSTS = ['xcrossword.herokuapp.com']
 
 
 # Application definition
@@ -142,14 +142,11 @@ EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 
 STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
-
-# STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
-STATICFILES_FINDERS = (
-    'django.contrib.staticfiles.finders.FileSystemFinder',
-    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
-)
+if DEBUG:
+   STATICFILES_DIRS = [
+   os.path.join(BASE_DIR, 'static'),
+   ]
+else:
+   STATIC_ROOT = os.path.join(BASE_DIR,'static')
 
 
