3cb4ef1c82e3a25bcfdafdab439bd4393873aa0f
Krazy0 <57904740+krazyyyy@users.noreply.github.com>
2021-04-20 10:43:13 +0500

    Update urls.py

--- crossword/urls.py
+++ crossword/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name="index"),
+    path('latest', views.getLatestPuzzle, name='latest'),
     path('puzzle/<str:pk>', views.renderPuzzle, name="puzzle"),
     path('saveprogress', views.saveProgess, name="saveprogress")
 ]
